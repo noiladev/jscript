cc06333acd5b82901bf881ae7d8bcfcc87b85e45
noiladev <noila10122009@gmail.com>
2023-09-27 07:22:52 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// raqam agar 18 dan katta bolsa aka
+// raqam agar 18 dan kichik bolsa uka
+// raqam agar 18 ga teng bolsa oshna
+
+alert("Assalomu alaykum, Hush kelibsiz")
+var age = prompt("Yoshingizni  kiriting...")
+
+if(age > 18 ) 
+{
+    console.log("Siz mendan kattasiz. Aka");
+}
+if( age < 18 ) 
+{
+    console.log("Siz mendan kichkinasiz Uka");
+}
+else if( age == 18) 
+{
+    console.log("bizning yoshimiz teng");
+}
